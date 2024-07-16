c5bd0047b076e7bb3a350f13ed00e7ec414e9115
artipandit6860 <artipandit6860@gmail.com>
2024-07-16 20:22:22 +0530

    first commit

---
+++ AddApp.java
@@ -0,0 +1,11 @@
+public class AddApp
+{
+   public static void main(String x[])
+   {
+       int a,b,c;
+	       a=Integer.parseInt(x[0]);
+		   b=Integer.parseInt(x[1]);
+		   c=a+b;
+		   System.out.printf("add is %d \n",c);
+      }
+}
---
+++ AllOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AllOdd
+{
+   public static void main(String x[])
+   {
+      int no,i;
+      Scanner xyz=new Scanner(System.in);
+	  System.out.print("Enter no");
+	  no=xyz.nextInt();
+	  for(i=1;i<=no;i=i+2)
+	  {
+	     System.out.print(i + " ");
+		 }
+		 }
+		 }
+		 
